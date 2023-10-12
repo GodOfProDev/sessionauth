b3da56a2cc417378813fc7781b2ad4331d801d5d
GodOfPro <1387ilia@gmail.com>
2023-10-13 01:56:20 +0330

    Add sql migration

---
+++ sql/migrations/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
